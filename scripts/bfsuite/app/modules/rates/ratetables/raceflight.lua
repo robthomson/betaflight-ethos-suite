9d9beb6e61aa2a49b8a50bfd98f3b4a4ae94da2b
Rob Thomson <rob@marotori.com>
2025-04-06 14:45:41 +0100

    Clone RF Rates

---
+++ scripts/bfsuite/app/modules/rates/ratetables/raceflight.lua
@@ -0,0 +1,41 @@
+local mspapi = {
+    api = {
+        [1] = 'RC_TUNING',
+    },
+    formdata = {
+        name = bfsuite.i18n.get("app.modules.rates.raceflight"),
+        labels = {
+        },
+        rows = {
+            bfsuite.i18n.get("app.modules.rates.roll"),
+            bfsuite.i18n.get("app.modules.rates.pitch"),
+            bfsuite.i18n.get("app.modules.rates.yaw"),
+            bfsuite.i18n.get("app.modules.rates.collective")
+        },
+        cols = {
+            bfsuite.i18n.get("app.modules.rates.rc_rate"),
+            bfsuite.i18n.get("app.modules.rates.acroplus"),
+            bfsuite.i18n.get("app.modules.rates.expo")
+        },
+        fields = {
+            -- rc rate
+            {row = 1, col = 1, min = 0, max = 100, default = 24, mult = 10, step = 10, mspapi = 1, apikey = "rcRates_1"},
+            {row = 2, col = 1, min = 0, max = 100, default = 24, mult = 10, step = 10, mspapi = 1, apikey = "rcRates_2"},
+            {row = 3, col = 1, min = 0, max = 100, default = 40, mult = 10, step = 10, mspapi = 1, apikey = "rcRates_3"},
+            {row = 4, col = 1, min = 0, max = 100, default = 50, decimals = 1, scale = 4, mspapi = 1, apikey = "rcRates_4"},
+            -- acro+
+            {row = 1, col = 2, min = 0, max = 255, default = 0, mspapi = 1, apikey = "rates_1"},
+            {row = 2, col = 2, min = 0, max = 255, default = 0, mspapi = 1, apikey = "rates_2"},
+            {row = 3, col = 2, min = 0, max = 255, default = 0, mspapi = 1, apikey = "rates_3"},
+            {row = 4, col = 2, min = 0, max = 255, default = 0, mspapi = 1, apikey = "rates_4"},
+            -- expo
+            {row = 1, col = 3, min = 0, max = 100, default = 0, mspapi = 1, apikey = "rcExpo_1"},
+            {row = 2, col = 3, min = 0, max = 100, default = 0, mspapi = 1, apikey = "rcExpo_2"},
+            {row = 3, col = 3, min = 0, max = 100, default = 0, mspapi = 1, apikey = "rcExpo_3"},
+            {row = 4, col = 3, min = 0, max = 100, default = 0, mspapi = 1, apikey = "rcExpo_4"}
+        }
+    }                 
+}
+
+
+return mspapi
