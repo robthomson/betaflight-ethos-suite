c01b894b30dc2b36b5caa488f5b3fb64b5dc02d3
Rob Thomson <rob@marotori.com>
2025-10-31 14:09:03 +0000

    Dashboard init (#9)

---
+++ scripts/bfsuite/tasks/events/init.lua
@@ -0,0 +1,9 @@
+--[[
+  Copyright (C) 2025 Rob Thomson
+  GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
+]] --
+
+local bfsuite = require("bfsuite")
+
+local init = {interval = 0.1, script = "events.lua", linkrequired = true, spreadschedule = true, simulatoronly = false}
+return init
