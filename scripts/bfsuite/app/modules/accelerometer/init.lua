d1e34fccb6df19782c7dc4d8e81a8b576053b830
Rob Thomson <rob@marotori.com>
2025-11-07 06:29:57 +0000

    Accel fix

--- scripts/bfsuite/app/modules/accelerometer/init.lua
+++ scripts/bfsuite/app/modules/accelerometer/init.lua
@@ -3,7 +3,7 @@
   GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
 ]] --
 
-local rfsuite = require("rfsuite")
+local bfsuite = require("bfsuite")
 
 local init = {title = "@i18n(app.modules.accelerometer.name)@", section = "hardware", script = "accelerometer.lua", image = "acc.png", order = 5, ethosversion = {1, 6, 2}}
 
